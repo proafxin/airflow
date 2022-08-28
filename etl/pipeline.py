42aa69564131747eabc031e7e09a14074d4d8bcc
proafxin <billalmasum93@gmail.com>
2022-08-29 04:38:35 +0600

    used sqlalchemy for connection

--- etl/pipeline.py
+++ etl/pipeline.py
@@ -6,10 +6,14 @@ import os
 
 import pandas as pd
 import pandera as pa
-from mysql.connector import Error, connect
+
+# from mysql.connector import Error, connect
 from pandera.typing import Series
+from sqlalchemy import create_engine
 
 from etl.calculate_mean import calculate_mean_by_col
+
+# from sqlalchemy.engine import URL
 
 
 def get_connection():
@@ -22,17 +26,22 @@ def get_connection():
     username = os.environ["MYSQL_USERNAME"]
     password = os.environ["MYSQL_PASSWORD"]
     port = os.environ["MYSQL_PORT"]
+    port = int(port)
+    print(port, type(port))
     dbname = os.environ["AIRFLOW_DBNAME"]
     host = os.environ["MYSQL_HOST"]
 
-    try:
-        connection = connect(
-            user=username, database=dbname, password=password, host=host, port=port
-        )
+    # connection_string = """DRIVER=\{ODBC Driver 17 for SQL Server};"""
+    # connection_string = (
+    #     f"{connection_string}SERVER={host};DATABASE={dbname};UID={username};PWD={password}"
+    # )
+    # connection_url = URL.create(f"mysql+pymysql://{username}:{password}@{host}:{port}/{dbname}")
+    engine = create_engine(
+        f"mysql+pymysql://{username}:{password}@{host}:{port}/{dbname}?charset=utf8"
+    )
 
-        return connection
-    except Error:
-        return None
+    connection = engine.connect()
+    return connection
 
 
 def read_table_from_db(table_name: str, connection):
@@ -68,21 +77,18 @@ class RatingSchema(pa.SchemaModel):
     movie_id: Series[int] = pa.Field(nullable=False, ge=1)
 
 
-def get_joined_data():
+def get_joined_data(connection):
     """Join movies and ratings based on movie_id
 
     :return: Joined dataframe.
     :rtype: pd.DataFrame
     """
 
-    conn = get_connection()
-
-    movies = read_table_from_db(table_name="movies", connection=conn)
+    movies = read_table_from_db(table_name="movies", connection=connection)
     MovieSchema.validate(movies)
 
-    ratings = read_table_from_db(table_name="ratings", connection=conn)
+    ratings = read_table_from_db(table_name="ratings", connection=connection)
     RatingSchema.validate(ratings)
-    conn.close()
 
     joined_data = pd.merge(
         left=movies,
@@ -95,22 +101,39 @@ def get_joined_data():
     return joined_data
 
 
-def get_means():
+def write_to_table(connection, table_name: str, data: pd.DataFrame):
+    """Write dataframe to table using SQLAlchemy connection
+
+    :param connection: SQLAlchemy connection
+    :type connection: connect
+    :param table_name: Table to write data
+    :type table_name: str
+    :param data: Dataframe to write
+    :type data: pd.DataFrame
+    """
+
+    data.to_sql(con=connection, name=table_name, if_exists="replace")
+
+
+def run_pipeline(connection):
     """Get average by movie and average by user
 
     :return: Dataframe of mean by movie, dataframe of mean by user
     :rtype: Tuple[pd.DataFrame, pd.DataFrame]
     """
 
-    data = get_joined_data()
+    data = get_joined_data(connection=connection)
     print(data)
     movie_mean = calculate_mean_by_col(data=data, groupby_cols=["movie_id"], groupby_on="rating")
     print(movie_mean)
     user_mean = calculate_mean_by_col(data=data, groupby_cols=["user_id"], groupby_on="rating")
     print(user_mean)
+    write_to_table(connection=connection, table_name="movie_avg", data=movie_mean)
+    write_to_table(connection=connection, table_name="user_avg", data=user_mean)
 
     return movie_mean, user_mean
 
 
 if __name__ == "__main__":
-    get_means()
+    conn = get_connection()
+    run_pipeline(connection=conn)
